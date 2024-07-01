b75350f112bb866988ba575d152e38dcfdcbdd0e
esther57-web <kamardine.ossete@gmail.com>
2024-07-01 10:39:34 +0200

    rating fixed and averageRating to fix

--- controllers/book.js
+++ controllers/book.js
@@ -4,12 +4,15 @@ const Book = require('../models/book');
 const fs = require('fs');
 
 //Initialise averageRating à 0 et le ratings avec un tableau vide
-// 1 rating max par userId
-//r
+// optimisation
 exports.createBook = (req, res, next) => {
   const bookObject = JSON.parse(req.body.book);
   delete bookObject._id;
   delete bookObject._userId;
+  myRate = bookObject.ratings
+  //bookObject.ratings = []
+  //bookObject.averageRating = 0
+  
   
   function averageCalcul(ratings) {
     const totalGrade = ratings.reduce((total, rate) => total + rate.grade, 0);
@@ -17,23 +20,19 @@ exports.createBook = (req, res, next) => {
     return average;
   }
 
-  const averageRating = averageCalcul(bookObject.ratings);
+  const averageRatingValue = averageCalcul(bookObject.ratings);
 
   const book = new Book({
       ...bookObject,
       userId: req.auth.userId,
       imageUrl: `${req.protocol}://${req.get('host')}/images/${req.file.filename}`,
-      ratings: [],
-      averageRating: 0
+      averageRating: averageRatingValue
   });
-  book.ratings.push(bookObject.ratings)
-  book.averageRating = averageRating;
   book.save()
   .then(() => res.status(201).json({ message: 'Objet enregistré !'})) 
   .catch(error => { res.status(400).json( { error })})
 };
 
-//réponse attendue : tableau de livres
 exports.getAllBooks = (req, res, next) => {
     Book.find().then(
       (books) => {
@@ -106,26 +105,22 @@ exports.getOneBook = (req, res, next) => {
       });
 };
 
-
-
 // L'ID de l'utilisateur et la note doivent être ajoutés au 
 //tableau "rating" afin de ne pas laisser un utilisateur
 // noter deux fois le même livre.
 // Il n’est pas possible de modifier une note.
-// La note moyen de "averageRating" doit être tenue à
-// jour, et le livre renvoyé en réponse de la requête.
+// La note moyen de "averageRating" doit être tenue à jour, et le livre renvoyé en réponse de la requête.
+
 
 exports.userRating = (req, res, next ) => {
   const oneRating = req.body
-  delete oneRating._id
   delete oneRating._userId
+  //delete oneRating._id
   oneRating.userId = req.auth.userId
-  if(oneRating.rating > 5 || oneRating.rating < 0) {
-    return res.status(400).json({ message: 'La note doit être comprise entre 0 et 5.' });
-  }
+ 
   Book.findOne({ _id: req.params.id })
     .then(book => {
-      if (book.ratings.userId.find(user => user === req.auth.userId)) {
+      if (book.ratings.userId === req.auth.userId) {
         return res.status(400).json({ message: 'Une seule note par utilisateur est autorisée.' });
       } else {
         book.ratings.push({
@@ -133,14 +128,12 @@ exports.userRating = (req, res, next ) => {
           grade: oneRating.rating
         });
         book.save()
-        console.log(book)
           .then(() => {
-            res.status(200).json({ message: 'Note ajoutée !', book });
+            res.status(200).json(book);
           })
           .catch(error => res.status(401).json({ error }));
       }
     })
-    console.log(book.ratings)
   .catch( error => {
     res.status(500).json({ error });
 });
@@ -149,7 +142,7 @@ exports.userRating = (req, res, next ) => {
 exports.bestThreeBooks = (req, res, next ) => {
   Book.find().then(
     (books) => {
-      const sortedBooks = books.sort((a, b) => b.averageRating - a.averageRating); // Trie les livres par rating décroissant
+      const sortedBooks = books.sort((a, b) => b.averageRating - a.averageRating); 
       const bestBooks = sortedBooks.slice(0, 3); 
       res.status(200).json(bestBooks );
     }
@@ -160,15 +153,5 @@ exports.bestThreeBooks = (req, res, next ) => {
       });
     }
   );
-  /*Book.find()
-    .then(books => {
-      const ratings = books.map(book => book.averageRating); 
-      const decreaseRatings = ratings.sort((a, b) => b - a);
-      const bestBooks = decreaseRatings.slice(0, 3);
-      res.status(200).json({ message: 'Voici les trois meilleurs livres !', books }); 
-    })
-  .catch( error => {
-    res.status(500).json({ error });
-});*/
 };
 
